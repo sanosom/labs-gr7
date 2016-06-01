e4eac15d05eb607255143b02dc83d94a3bd10947
Santiago Osorio <santiagoosorio1@gmail.com>
2016-06-01 18:10:13 -0500

    Added notifications demo

--- Organizate/app/src/main/java/organizate/compumovil/udea/edu/co/activities/MainActivity.java
+++ Organizate/app/src/main/java/organizate/compumovil/udea/edu/co/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package organizate.compumovil.udea.edu.co.activities;
 
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.ListView;
 
@@ -40,6 +42,19 @@ public class MainActivity extends AppCompatActivity {
         adapter = new EventCursor(this, activities, 0);
 
         listView.setAdapter(adapter);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_alarm)
+                .setProgress(100, 10, false)
+                .addAction(R.drawable.ic_alarm_off, "Terminar", null)
+                .addAction(R.drawable.ic_alarm_add, "Agregar 10 minutos", null)
+                .setContentTitle("Actividad 2")
+                .setContentText("Universidad de Antioquia - 27 minutos restantes");
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(1, mBuilder.build());
     }
 
     public void addEvent(View view) {
